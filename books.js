f78e294b5428a440fe24eabbd69a2d4b17ca3d6e
Nellius Kyalo <nelliuskyalo@gmail.com>
2023-02-28 15:15:47 +0300

    feat(add): Challenge 1 submission

---
+++ books.js
@@ -0,0 +1,41 @@
+//Library - getting the number of books sold in a store. Practice on JS classes
+
+//JS Class
+class Book {
+    constructor(title,author, isbn, numCopies) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+      this.numCopies = numCopies;
+    }
+    // Getter method to check for availability
+    get availability() {
+      if(this.numCopies == 0){
+        return "Out of stock";
+      }else if(this.numCopies < 10){
+        return "Low stock";
+      }else{
+        return "In stock";
+      }
+    }
+
+    //function to get copies sold
+    sell(numSold){
+        return this.numCopies - numSold;
+    }
+
+    //function to get copies restocked
+    restock(numCopies){
+        return numCopies + this.numCopies;
+    }
+  }
+  
+  const Book1 = new Book("Tupende","Mark",267805,10);
+
+  
+  console.log(Book1.availability); 
+  console.log(Book1.sell(6));
+  console.log(Book1.restock(10));
+
+
+  
