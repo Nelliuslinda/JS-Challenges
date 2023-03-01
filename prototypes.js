420d27a279033edbfa3353c75ff6c574bdad7ad4
Nellius Kyalo <nelliuskyalo@gmail.com>
2023-03-01 12:07:33 +0300

    feat(add): alternative solution to JS challenge 2

--- prototypes.js
+++ prototypes.js
@@ -7,15 +7,30 @@ class Movie{
     this.releaseYear = releaseYear;
     this.rating = rating;
  }
+
+    getOverview(){
+        return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It recieved a rating of ${this.rating}`;
+    }
 }
 
-//Prototype to display rating
- Movie.prototype.review = function(){
+/*Alternative Solution -> when creating the movie object using a function
+
+function Movie(title, director, genre, releaseYear, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+}
+ Movie.prototype.getOverview = function(){
     return ' ""' + this.title + ', a ' + this.genre + ' film directed by ' + this.director + 
     ' was released in '+ this.releaseYear + '. It received a rating of ' + this.rating + '"" ';
  }
 
- const Movie1 = new Movie("Avatar" , "John Lee" , "action", 2009, 4.5);
- console.log(Movie1.review());
+*/ 
 
- 
+
+ const Movie1 = new Movie("Avatar" , "John Lee" , "action", 2009, 4.5);
+ console.log(Movie1.getOverview());
+
+
